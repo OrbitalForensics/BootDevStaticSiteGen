c7433b4335d68584967a6ae5a2f1274b05a90452
TheGhostInTheCloud <jptyrrell90@gmail.com>
2025-07-06 15:54:24 -0400

    6 Jul 2025 15:54 EST, Ch2 nodes L1 TextNodes

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("some text", TextType.BOLD, "http://example.com")
+    print(node)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
